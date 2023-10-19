ea34ed6e1550c78034f824530326fb59a576f8d5
direct66612 <direct66612@gmail.com>
2023-10-19 13:47:43 -0400

    update

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header';
+import './js/exercises-aside';
+import './js/exercises-main';
+import './js/favorites';
+import './js/footer';
+import './js/loader';
+import './js/scroll-down';
+import axios from 'axios';
+import Notiflix from 'notiflix';
