8d370dea31515841f4cbae6d18ba946c4957fd04
direct66612 <direct66612@gmail.com>
2023-10-20 08:39:42 -0400

    update api.js

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,6 +1,7 @@
 import '../api-service/exercises-api';
 import '../api-service/categories-api';
 import '../templates/exercises-markup';
+import axios from 'axios';
 
 export const BASE_URL = 'https://your-energy.b.goit.study/api';
 axios.defaults.baseURL = BASE_URL;
