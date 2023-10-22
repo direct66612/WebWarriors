346d1774a2e3ecc2cdc70cdb352c0401a1eeae62
Vitalii1973 <protsyukv@ukr.net>
2023-10-22 18:39:59 +0300

    feature/footer-1

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,34 +1,44 @@
-// для обробки кліку на логотипі
-document.getElementById('logo').addEventListener('click', function () {
-  window.location.href = '../index.html';
-});
-
-// код для обробки форми підписки
-document
-  .getElementById('subscription-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault(); // Заборонити відправку форми
-
-    const emailInput = document.getElementById('email-input');
-    const email = emailInput.value;
-
-    // Отправити дані на сервер
-    fetch('https://your-energy.b.goit.study/api/subscription', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: email }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          alert('You have successfully subscribed to receive new exercises!');
-        } else {
-          alert('Subscription error. Please try again.');
-        }
-      })
-      .catch(error => {
-        alert('Request error. Please try again later.');
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('logo-link').addEventListener('click', function () {
+    window.location.href = '../index.html';
   });
+
+  // код для обробки форми підписки
+  document
+    .getElementById('subscription-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault(); // Заборонити відправку форми
+
+      const emailInput = document.getElementById('email-input');
+      const email = emailInput.value;
+      console.log(email);
+
+      // Виконуємо мінімальну валідацію поля email
+      if (!emailInput.checkValidity()) {
+        alert('https://your-energy.b.goit.study/api/subscription');
+        return;
+      }
+
+      // Отправити дані на сервер
+      fetch('https://your-energy.b.goit.study/api/subscription', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.message);
+          if (data.message === 'Service not found') {
+            throw Error('Request error. Please try again later.');
+          } else {
+            alert('You have successfully subscribed to receive new exercises!');
+            //alert('Subscription error. Please try again.');
+          }
+        })
+        .catch(error => {
+          alert(error);
+        });
+    });
+});
