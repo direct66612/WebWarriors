2d35e2802c6fa721cbfe6222357abeca56903588
direct66612 <direct66612@gmail.com>
2023-10-24 18:25:27 -0400

    add main js file in favorites and fix img in modal-ex

---
+++ src/favorites-main.js
@@ -0,0 +1,5 @@
+import './js/header';
+import './js/exercises-aside';
+import './js/exercises-main';
+import './js/favorites';
+import './js/loader';
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './js/header';
 import './js/exercises-aside';
 import './js/exercises-main';
-import './js/favorites';
 import './js/footer';
 import './js/scroll-up';
 import './js/loader';
@@ -11,6 +10,6 @@ import './js/api-service/exercises-api';
 import './js/api-service/categories-api';
 import './js/templates/exercises-markup';
 import './js/exercises';
-import './js/exercises-category.js'
+import './js/exercises-category.js';
 import './js/templates/exercises-category-markup.js';
 import './js/modal-ex';
